f193d1320770c10f8771f833c541b3e0a6dbc5bb
Matěj Bartoň <bartom47@fel.cvut.cz>
2021-01-27 12:34:05 +0100

    🎨 Implements styles for generated content.

--- src/templates/Category.jsx
+++ src/templates/Category.jsx
@@ -12,12 +12,12 @@ class Category extends React.Component{
                     <img src="https://i.pinimg.com/originals/b5/60/ca/b560ca9bd65061bda698321a17d22f34.jpg" alt="heading of article" className="object-cover w-screen z-0"/>
                 </div>
                 <div className="md:absolute md:z-20 md:mt-32 flex-row space-y-0">
-                    <main id="category" className="md:mx-auto md:max-w-2xl px-10 py-5 md:mb-32 bg-black bg-opacity-80 text-gray-200 font-mono text-sm md:text-base font-extralight">                    
+                    <main id="category" className="md:mx-auto md:max-w-2xl px-10 py-5 md:mb-32 dark-colors font-mono text-sm md:text-base font-extralight">                    
                         <h1 className="text-center font-sans font-light uppercase text-4xl md:text-5xl my-5">Rubrika</h1>
-                        <section id="description">
+                        <section id="description" className="typography dark-colors">
                             <h2 className="sr-only">Popis rubriky</h2>
                             <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus risus enim, bibendum eu neque quis, aliquam tincidunt mi. Vivamus quis hendrerit sapien. Quisque semper mauris ut massa commodo rhoncus. Curabitur nec velit eget augue rhoncus ullamcorper vel id ex. Integer suscipit nisl ac quam vestibulum feugiat. Integer bibendum mauris suscipit aliquet ornare. In vestibulum odio ut tortor malesuada, et porttitor velit imperdiet.
+                                Jenže <strong>kvůli všudy</strong> <a href="/#">Link</a> <em>přítomné trávě</em> jsou stíny balónků sotva vidět, natož aby šlo rozeznat, jakou barvu tyto stíny mají. Uvidět tak balónky náhodný kolemjdoucí, jistě by si pomyslel, že už tu takhle poletují snad tisíc let. Stále si víceméně drží výšku a ani do stran se příliš nepohybují. Proti slunci to vypadá, že se slunce pohybuje k západu rychleji než balónky, a možná to tak skutečně je. Nejeden filozof by mohl tvrdit, že balónky se sluncem závodí, ale fyzikové by to jistě vyvrátili. Z fyzikálního pohledu totiž balónky působí zcela nezajímavě.
                             </p>
                         </section>                        
                     </main>
--- src/templates/FrontPage.jsx
+++ src/templates/FrontPage.jsx
@@ -12,10 +12,10 @@ class FrontPage extends React.Component{
                     <img src="https://i.pinimg.com/originals/b5/60/ca/b560ca9bd65061bda698321a17d22f34.jpg" alt="heading of article" className="object-cover w-screen z-0"/>
                 </div>
                 <div className="md:absolute md:z-20 md:mt-32 flex-row space-y-0">
-                    <main id="frontPage" className="md:mx-auto md:max-w-2xl px-10 py-5 md:mb-32 bg-black bg-opacity-80 text-gray-200 font-mono text-sm md:text-base font-extralight">                    
+                    <main id="frontPage" className="md:mx-auto md:max-w-2xl px-10 py-5 md:mb-32 typography dark-colors font-mono text-sm md:text-base font-extralight">                    
                         <h1 className="text-center font-sans font-light uppercase text-4xl md:text-5xl my-5">Uvítací text</h1>
                         <p>
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus risus enim, bibendum eu neque quis, aliquam tincidunt mi. Vivamus quis hendrerit sapien. Quisque semper mauris ut massa commodo rhoncus. Curabitur nec velit eget augue rhoncus ullamcorper vel id ex. Integer suscipit nisl ac quam vestibulum feugiat. Integer bibendum mauris suscipit aliquet ornare. In vestibulum odio ut tortor malesuada, et porttitor velit imperdiet. Duis varius, odio auctor posuere tincidunt, lectus dolor semper odio, vel tempus arcu arcu sit amet quam. 
+                        Jenže <strong>kvůli všudy</strong> <a href="/#">Link</a> <em>přítomné trávě</em> jsou stíny balónků sotva vidět, natož aby šlo rozeznat, jakou barvu tyto stíny mají. Uvidět tak balónky náhodný kolemjdoucí, jistě by si pomyslel, že už tu takhle poletují snad tisíc let. Stále si víceméně drží výšku a ani do stran se příliš nepohybují. Proti slunci to vypadá, že se slunce pohybuje k západu rychleji než balónky, a možná to tak skutečně je. Nejeden filozof by mohl tvrdit, že balónky se sluncem závodí, ale fyzikové by to jistě vyvrátili. Z fyzikálního pohledu totiž balónky působí zcela nezajímavě.
                         </p>                      
                     </main>
                     <PostContainer id="posts" title="Nejnovější články" className="bg-white h-full py-10 md:px-10"/>
